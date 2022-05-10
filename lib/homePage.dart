6c7bf5cd085645e21efc3160026c05394cee4ee5
akashangadii <akashakku2202@gmail.com>
2022-05-10 14:25:37 +0530

    changed appbar title

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -16,7 +16,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            "Kidzee",
+            "Kidzee Sample",
             textAlign: TextAlign.center,
           ),
         ),
